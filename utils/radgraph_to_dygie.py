65d2dc34366f5128cf2220255435a76d63633610
Abubakar Ahmad <abubabakr@gmail.com>
2022-09-25 17:48:56 +0300

    Utils: added functions to convert datasets from RadGraph format to DYGIE++

--- utils/radgraph_to_dygie.py
+++ utils/radgraph_to_dygie.py
@@ -3,15 +3,68 @@ import jsonlines
 import random
 import fire
 
-def run(input_files=["train"], output_file="train", no_of_samples=600):
+
+def one_sentence(input_files=["train"], output_file="train", no_of_samples=600):
     data = {}
     for file_name in input_files:
-        with open('data/radgraph/' + file_name + '.json') as file:
+        with open('data/radgraph_1/' + file_name + '.json') as file:
             data = json.load(file)
 
     dygie_reports = []
     report_names = list(data.keys())
-    # random.shuffle(report_names)
+    random.shuffle(report_names)
+    count = 0
+    for report_name in report_names:
+        report = data[report_name]
+        tokenized_report = report["text"].split()
+
+        ner = [[]]
+        relations = [[]]
+        if "entities" in report:
+            entities = report["entities"]
+        else:
+            entities = report["labeler_1"]["entities"]
+
+        for i in entities:
+            entity = entities[i]
+            ner[0].append([
+                entity["start_ix"],
+                entity["end_ix"],
+                entity["label"]
+            ])
+            entity_relations = [[
+                entity["start_ix"],
+                entity["end_ix"],
+                entities[relation[1]]["start_ix"],
+                entities[relation[1]]["end_ix"],
+                relation[0]
+            ] for relation in entity["relations"]]
+            relations[0] = relations[0] + entity_relations
+
+        dygie_reports.append({
+            "doc_key": report_name,
+            "sentences": [tokenized_report],
+            "ner": ner,
+            "relations": relations
+        })
+        count += 1
+        if count == no_of_samples:
+            break
+
+    with jsonlines.open('data/' + output_file + '.json', 'w') as writer:
+        writer.write_all(dygie_reports)
+    print(str(count) + ' samples converted. ' + str(input_files) + ' -> ' + output_file)
+
+
+def multiple_sentences(input_files=["train"], output_file="train", no_of_samples=600):
+    data = {}
+    for file_name in input_files:
+        with open('data/radgraph_1/' + file_name + '.json') as file:
+            data = json.load(file)
+
+    dygie_reports = []
+    report_names = list(data.keys())
+    random.shuffle(report_names)
     count = 0
     for report_name in report_names:
         report = data[report_name]
@@ -29,14 +82,12 @@ def run(input_files=["train"], output_file="train", no_of_samples=600):
             entities = report["labeler_1"]["entities"]
 
         sentence_index = 0
-        sum_of_prev_tokens = 0
         for i in entities:
             entity = entities[i]
             while entity["start_ix"] >= sum(tokens_count[:sentence_index+1]):
                 sentence_index += 1
-                sum_of_prev_tokens = sum(tokens_count[:sentence_index])
-            start_ix = entity["start_ix"] - sum_of_prev_tokens
-            end_ix = entity["end_ix"] - sum_of_prev_tokens
+            start_ix = entity["start_ix"]
+            end_ix = entity["end_ix"]
             ner[sentence_index].append([
                 start_ix,
                 end_ix,
@@ -45,8 +96,8 @@ def run(input_files=["train"], output_file="train", no_of_samples=600):
             entity_relations = [[
                 start_ix,
                 end_ix,
-                entities[relation[1]]["start_ix"] - sum_of_prev_tokens,
-                entities[relation[1]]["end_ix"] - sum_of_prev_tokens,
+                entities[relation[1]]["start_ix"],
+                entities[relation[1]]["end_ix"],
                 relation[0]
             ] for relation in entity["relations"]]
             relations[sentence_index] = relations[sentence_index] + entity_relations
@@ -63,8 +114,6 @@ def run(input_files=["train"], output_file="train", no_of_samples=600):
 
     with jsonlines.open('data/' + output_file + '.json', 'w') as writer:
         writer.write_all(dygie_reports)
-    # with open('data/' + output_file + '.json', 'w', encoding='utf-8') as writer:
-    #     json.dump(dygie_reports, writer, ensure_ascii=False, indent=4)
     print(str(count) + ' samples converted')
 
 
